14fa39d7b1aad7eed61b4c465181c583cedc72ef
kalvin chen <kalvin412@gmail.com>
2023-12-02 15:12:51 +0800

    modified the welcome message to be hardcoded

--- resources/views/layouts/navbars/auth/topnav.blade.php
+++ resources/views/layouts/navbars/auth/topnav.blade.php
@@ -8,7 +8,7 @@
                 <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
                 <li class="breadcrumb-item text-sm text-white active" aria-current="page">{{ $title }}</li>
             </ol>
-            <h1 class="font-weight-bolder text-white mb-0">{{ $welcome_message }}</h1>
+            <h1 class="font-weight-bolder text-white mb-0">Welcome to FKKMS!</h1>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">         
