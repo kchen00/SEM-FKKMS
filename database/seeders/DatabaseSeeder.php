cd60e8b978d9046d06933d213084e2b7e6351ead
chuaK <chuakianpheng@gmail.com>
2023-12-16 15:26:14 +0800

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,34 @@ class DatabaseSeeder extends Seeder
     {
         DB::table('users')->insert([
             'username' => 'admin',
-            'firstname' => 'Admin',
-            'lastname' => 'Admin',
+            'phone_num' => '0123456789',
             'email' => 'admin@argon.com',
+            'role' => 'admin',
             'password' => bcrypt('secret')
+        ]);
+        DB::table('users')->insert([
+            'username' => 'student',
+            'phone_num' => '0123456781',
+            'email' => 'student@argon.com',
+            'role' => 'student',
+            'password' => bcrypt('1234')
+        ]);
+        DB::table('users')->insert([
+            'username' => 'vendor',
+            'phone_num' => '0123456782',
+            'email' => 'vendor@argon.com',
+            'role' => 'vendor',
+            'password' => bcrypt('1234')
+        ]);
+ 
+        DB::table('participants')->insert([
+            'user_ID' => '2',
+        ]);
+        DB::table('participants')->insert([
+            'user_ID' => '3',
+        ]);
+        DB::table('participants')->insert([
+            'user_ID' => '3',
         ]);
     }
 }
