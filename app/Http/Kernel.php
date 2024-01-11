31dc8513062595bd41194436c1658393cf03c598
kalvin chen <kalvin412@gmail.com>
2024-01-12 03:35:00 +0800

    added CheckApplication middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\CheckApplication;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'firstTimeLogin' => \App\Http\Middleware\CheckFirstTimeLogin::class,
+        'checkApplication' => \App\Http\Middleware\checkApplication::class,
     ];
 }
