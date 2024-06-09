e54bfad6ceb9c0df69b3c95f45a4e901fba9a3ee
Decker7 <afiqm6526@gmail.com>
2024-06-09 23:35:17 +0800

    afiq module

--- resources/views/ManageComplaint/complaintform.blade.php
+++ resources/views/ManageComplaint/complaintform.blade.php
@@ -3,35 +3,38 @@
 @section('content')
 @include('layouts.navbars.auth.topnav', ['title' => 'Complaint form', 'welcome_message' => 'Complaint form'])
 <!-- if the user has not yet applied for KIOSK, render this-->
-<div class="container-fluid py-4 row">
+<div class="container-fluid py-4 row ">
     <div class="card col">
-        <div class="card-body text-center">
-            <h5 class="card-title">Customer Information</h5>
-            <form id="fs-frm" name="complaint-form" accept-charset="utf-8" action="{{ route('complaint-form.store') }}" method="post">
-               @csrf
-               <input type="hidden" name="parti_ID"  class="form-control" value="{{ $user->user_ID }}">
-               <!-- complaintform.blade.php -->
-               <div class="form-group">
-                <label for="example-fullname-input" class="form-control-label">Full name</label>
-                <input type="FullName" name="name" id="full-name" class="form-control" placeholder="First and Last" required="" value="{{ $user->username ?? '' }}">
-            </div>
-            <div class="form-group">
-                <label for="example-email-input" class="form-control-label">Email</label>
-                <input type="Email" name="_replyto" id="email-address" class="form-control" placeholder="email@domain.tld" required="" value="{{ $user->email ?? '' }}">
-            </div>
-            <div class="form-group">
-                <label for="example-tel-input" class="form-control-label">Phone</label>          
-                <input type="phoneNum" name="phoneNum" id="telephone" class="form-control" placeholder="(555) 555-5555" value="{{ $user->phone_num ?? '' }}">
-            </div>
-            <div class="form-group">
-                <label for="example-text-input" class="form-control-label">Complaint title</label>
-                <input class="form-control" type="text" name="complaintTitle" id="example-text-input" required>
-           </div>
-           <div class="form-group">
-                <label for="exampleFormControlTextarea1">Complaint details</label>
-                <textarea class="form-control" name="complaintDetails" id="exampleFormControlTextarea1" rows="3" required></textarea>
-           </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="card-body text-center" style="font-family: Arial, Helvetica, sans-serif;">
+            <h4 class="card-title " style="margin-bottom: 30px;">Customer Complaint Form </h4>
+            <form id="fs-frm" name="complaint-form" class="row g-3" accept-charset="utf-8" action="{{ route('complaint-form.store') }}" method="post">
+                @csrf
+                <input type="hidden" name="parti_ID" class="form-control" value="{{ $user->user_ID }}">
+                <!-- complaintform.blade.php -->
+                <div class="form-group col-md-5" style="text-align: start">
+                    <label for="example-fullname-input" class="form-control-label form-label">Full name</label>
+                    <input type="FullName" name="name" id="full-name" class="form-control" placeholder="First and Last" required="" value="{{ $user->username ?? '' }}">
+                </div>
+                <div class="form-group col-md-4" style="text-align: start;">
+                    <label for="example-email-input" class="form-control-label">Email</label>
+                    <input type="Email" name="_replyto" id="email-address" class="form-control" placeholder="email@domain.tld" required="" value="{{ $user->email ?? '' }}">
+                </div>
+                <div class="form-group col-md-3 " style="text-align: start;">
+                    <label for="example-tel-input" class="form-control-label">Phone</label>
+                    <input type="phoneNum" name="phoneNum" id="telephone" class="form-control" placeholder="(555) 555-5555" value="{{ $user->phone_num ?? '' }}">
+                </div>
+                <div class="form-group col-md-12" style="text-align:start">
+                    <label for="example-text-input" class="form-control-label ">Complaint title</label>
+                    <input class="form-control" type="text" name="complaintTitle" id="example-text-input" required>
+                </div>
+                <div class="form-group" style="text-align: start;">
+                    <label for="exampleFormControlTextarea1">Complaint details</label>
+                    <textarea class="form-control" name="complaintDetails" id="exampleFormControlTextarea1" rows="3" required></textarea>
+                </div>
+                <div style="display: flex; justify-content: flex-end;">
+                    <button type="submit" class="btn btn-primary col-md-2">Submit</button>
+                </div>
+
             </form>
         </div>
     </div>
--- resources/views/ManageComplaint/complaintmenu.blade.php
+++ resources/views/ManageComplaint/complaintmenu.blade.php
@@ -1,40 +1,59 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
-@include('layouts.navbars.auth.topnav', ['title' => 'Manage complaint', 'welcome_message' => ''])
-<!-- if the user has not yet applied for KIOSK, render this -->
+    @include('layouts.navbars.auth.topnav', ['title' => 'Manage complaint', 'welcome_message' => ''])
+    <!-- if the user has not yet applied for KIOSK, render this -->
 
 
-<!-- if the user has KIOSK, render this -->
-<div class="container-fluid py-4 row justify-content-center">
-    <div class="card col-md-4 m-4 text-center">
-        <div class="card-body">
-            <h5 class="card-title">New Complaint</h5>
-            <img src="{{ asset('img/createcomplaint.png') }}" alt="Create Complaint" class="img-fluid mx-auto d-block" style="width: 200px;">
+    <!-- if the user has KIOSK, render this -->
+    <div class="container-fluid py-4 row justify-content-center">
+        <div class="card col-md-4 m-4 text-center">
+            <div class="card-body">
+                <h5 class="card-title">New Complaint</h5>
+                <img src="{{ asset('img/createcomplaint.png') }}" alt="Create Complaint" class="img-fluid mx-auto d-block"
+                    style="width: 200px;">
 
-            <!-- Update the "Create" button href to direct to the complaint-form route -->
-            <div class="mt-3 d-flex justify-content-center">
-                <a href="{{ route('complaint-form') }}" class="btn btn-primary">Create</a>
+                <!-- Update the "Create" button href to direct to the complaint-form route -->
+                <div class="mt-3 d-flex justify-content-center">
+                    <a href="{{ route('complaint-form') }}" class="btn btn-primary">Create</a>
+                </div>
+                {{-- this is the div for the hit button for create complain form  --}}
+                <div class="mt-3">
+                    <span class="hint-circle" data-toggle="tooltip" data-placement="right"
+                        title="Click 'Create' to fill out the complaint form. Ensure all required fields are completed.">
+                        ?
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="card col-md-4 m-4 text-center">
+            <div class="card-body">
+                <h5 class="card-title">View Complaint</h5>
+                <img src="{{ asset('img/listcomplaint.png') }}" alt="List Complaint" class="img-fluid mx-auto d-block"
+                    style="width: 200px;">
+                <div class="mt-3 d-flex justify-content-center">
+                    <a href="{{ route('complaint-shows') }}" class="btn btn-primary">View</a>
+                </div>
+                {{-- this is the div for the hit button for view complain form  --}}
+                <div class="mt-3">
+                    <span class="hint-circle" data-toggle="tooltip" data-placement="right"
+                        title="Click 'View' to see list of complaints.">
+                        ?
+                    </span>
+                </div>
             </div>
         </div>
     </div>
-    <div class="card col-md-4 m-4 text-center">
-        <div class="card-body">
-            <h5 class="card-title">View Complaint</h5>
-            <img src="{{ asset('img/listcomplaint.png') }}" alt="List Complaint" class="img-fluid mx-auto d-block" style="width: 200px;">
-            <div class="mt-3 d-flex justify-content-center">
-                <a href="{{ route('complaint-shows') }}" class="btn btn-primary">View</a>
-            </div>
-        </div>
-    </div>
-</div>
 
-@include('layouts.footers.auth.footer')
+    @include('layouts.footers.auth.footer')
 @endsection
 
 @push('js')
-<script src="./assets/js/plugins/chartjs.min.js"></script>
-<script>
-    // ... (your existing JavaScript code)
-</script>
+    {{-- this is the java script for hover the hint button --}}
+    <script src="./assets/js/plugins/chartjs.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
 @endpush
